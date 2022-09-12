335f5fe77fba8e1b1ac40a9ca94be45d19dfd705
habib-ullah <habib4811@gmail.com>
2022-09-12 22:42:20 +0600

    added card

--- src/Components/CardSlider.jsx
+++ src/Components/CardSlider.jsx
@@ -2,7 +2,7 @@ import Card from "./Card"
 
 export default function CardSlider({data, title}) {
      console.log(data);
-  return <div>
+  return <div className="flex">
   {data.map((movie, index) => {
             return <Card movieData={movie} index={index} key={movie.id} />;
           })}
